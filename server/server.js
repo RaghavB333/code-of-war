71196a2c67df37d58204e72702e78dce2a725e12
RaghavBhargava <raghavbhargava3@gmail.com>
2025-06-17 10:33:20 +0530

    deployment-adjustments

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
