2378c326f853740d9a5fc6499b36c4a79fce9165
RaghavBhargava <raghavbhargava3@gmail.com>
2026-01-11 14:04:48 +0530

    fixed judge0 execution

--- app/api/judge0/status/route.js
+++ app/api/judge0/status/route.js
@@ -1,7 +1,10 @@
 export async function POST(req) {
   const { token } = await req.json();
 
-  const res = await fetch(`http://43.204.216.76:2358/submissions/${token}`);
+  const res = await fetch(
+    `http://${process.env.Judge0_IP}/submissions/${token}?base64_encoded=false`
+  );
+
   const data = await res.json();
   return Response.json(data);
 }
--- app/api/judge0/submit/route.js
+++ app/api/judge0/submit/route.js
@@ -1,9 +1,12 @@
 export async function POST(req) {
   const body = await req.json();
-  const res = await fetch("http://3.110.43.197:2358/submissions", {
+
+  const res = await fetch(`http://${process.env.Judge0_IP}/submissions?base64_encoded=false&wait=true`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
   });
 
   const data = await res.json();
